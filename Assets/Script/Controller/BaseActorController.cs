97186aab3a2272fc81cb5a8a62a79b57a4b32388
Thirtytwo <3267207337@qq.com>
2021-08-30 21:00:00 +0800

    fixed move

--- Assets/Script/Controller/BaseActorController.cs
+++ Assets/Script/Controller/BaseActorController.cs
@@ -9,7 +9,12 @@ public class BaseActorController : MonoBehaviour
     private bool acceptToClick;//允许点击进行移动
     //地面检测以及鼠标射线检测
     public List<HexInfo> path = new List<HexInfo>();
-    private float distance = 30f;
+    //移动参数
+    protected float speed = 2f;
+
+    [Header("信号及开关")]
+    public bool isThisActorRound;//当是这个角色的回合
+    public bool isMoving;//角色移动中
 
     //直接在Base类中调用Update和Start，避免每个角色通用功能代码调用重复
     private void Start()
@@ -20,55 +25,55 @@ public class BaseActorController : MonoBehaviour
 
     private void FixedUpdate()
     {
-    //Debug.Log(new Vector3(transform.position.x, -1, transform.position.z));
-    #region 检测
-    //鼠标停留在同一个方块
-        if (currentMouseStay == MouseRaySenser() && currentMouseStay != null)
+        //当是你的回合是才去检测，假设都选择移动
+        if (isThisActorRound)
         {
-            if (Input.GetMouseButtonDown(0) && acceptToClick)
+             //鼠标停留在同一个方块
+            if (currentMouseStay == MouseRaySenser() && currentMouseStay != null)
             {
-                acceptToClick = false;
-                for (int i = 0; i < path.Count; ++i)
+                if (Input.GetMouseButtonDown(0) && acceptToClick)
                 {
-                    transform.position = path[i].transform.position + new Vector3(0, 1, 0);
-                    currentMouseStay = path[i];
+                    ActorMove();
                 }
-                acceptToClick = true;
             }
-        }
-        //鼠标停留发生了变化
-        else if(currentMouseStay != MouseRaySenser() && MouseRaySenser() != null)
-        {
-            //先将上次记录删除
-            for (int i = 0; i < path.Count; ++i)
+            //鼠标停留发生了变化
+            else if(currentMouseStay != MouseRaySenser() && MouseRaySenser() != null && !isMoving)
             {
-                path[i].isSelected = false;
-            }
-            path.Clear();
-        //当路径为空菜去添加，避免重复添加导致的内存泄露
-        if (path.Count == 0)
-            {
-                Debug.Log(ActorGroundSenser());
-                path = MapManager.GetInstance().AStarFindWay(ActorGroundSenser(), MouseRaySenser());
-                for (int i = 0; i < path.Count; ++i)
+                try
                 {
-                    path[i].isSelected = true;
+                    //先将上次记录删除
+                    for (int i = 0; i < path.Count; ++i)
+                    {
+                        path[i].isSelected = false;
+                    }
+                    path.Clear();
                 }
+                catch (System.Exception)
+                {
+                    path.Clear();
+                }
+            //当路径为空菜去添加，避免重复添加导致的内存泄露
+            if (path.Count == 0)
+                {
+                    path = MapManager.GetInstance().AStarFindWay(ActorGroundSenser(), MouseRaySenser());
+                    for (int i = 0; i < path.Count; ++i)
+                    {
+                        path[i].isSelected = true;
+                    }
     
+                }
+                currentMouseStay = MouseRaySenser();
             }
-            currentMouseStay = MouseRaySenser();
-        }
-        else
-        {
+            else
+            {
         
+            }
         }
-    #endregion
     }
     #region 射线检测类
     //地面检测，并且传回HexInfo类
     private HexInfo ActorGroundSenser()
     {
-        Debug.Log("调用角色检测");
         Ray ray = new Ray(transform.position, -Vector3.up);
         RaycastHit hit;
         if(Physics.Raycast(ray,out hit,10f))
@@ -77,7 +82,6 @@ public class BaseActorController : MonoBehaviour
         }
         else
         {
-            //Debug.LogWarning("没有检测到地面");
             return null;
         }
     }
@@ -91,14 +95,32 @@ public class BaseActorController : MonoBehaviour
         }
         else
         {
-            //Debug.Log("鼠标没检测到物体");
             return null;
         }
     }
     #endregion
 
     #region 角色移动
+    protected void ActorMove()
+    {
+        StartCoroutine(Move(path.ToArray(), 0));
+    }
+    IEnumerator Move(HexInfo[] _path, int index)
+    {
+        while(transform.position != _path[_path.Length - 1].transform.position)
+        {
+            isMoving = true;
+            transform.position = Vector3.MoveTowards(transform.position, _path[index].transform.position + new Vector3(0, 1, 0), speed * Time.deltaTime);
+            if(Vector3.Distance(transform.position, _path[index].transform.position + new Vector3(0, 1, 0)) <= 0.2f)
+            {
+                index++;
+            }
+            yield return null;
+        }
+        isMoving = false;
+        yield return null;
 
+    }
     #endregion
 
     private void OnDrawGizmos()
